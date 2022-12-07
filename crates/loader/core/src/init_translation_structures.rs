c428e1ed822b4a48b95168f3c0b8d9c2e03ac4f2
Nick Spinale <nick@nickspinale.com>
2022-12-07 03:14:17 +0000

    loader: Clean up translation structure initialization

--- crates/loader/core/src/init_translation_structures.rs
+++ crates/loader/core/src/init_translation_structures.rs
@@ -1,3 +1,45 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+const PAGE_BITS: u64 = 12;
+const LEVEL_BITS: u64 = 9;
+
+const ARM_1GB_BLOCK_BITS: u64 = 30;
+const ARM_2MB_BLOCK_BITS: u64 = 21;
+const ARM_4KB_BLOCK_BITS: u64 = 12;
+
+const fn bit(i: u64) -> u64 {
+    1 << i
+}
+
+const PGD_LEVEL_FROM_LSB: u64 = 3;
+const PUD_LEVEL_FROM_LSB: u64 = 2;
+const PD_LEVEL_FROM_LSB: u64 = 1;
+const PT_LEVEL_FROM_LSB: u64 = 0;
+
+const fn get_level_index(level_from_lsb: u64, addr: u64) -> u64 {
+    (addr >> (LEVEL_BITS * level_from_lsb + PAGE_BITS)) & ((1 << LEVEL_BITS) -1)
+}
+
+const fn get_pgd_index(addr: u64) -> u64 {
+    get_level_index(PGD_LEVEL_FROM_LSB, addr)
+}
+
+const fn get_pud_index(addr: u64) -> u64 {
+    get_level_index(PUD_LEVEL_FROM_LSB, addr)
+}
+
+const fn get_pd_index(addr: u64) -> u64 {
+    get_level_index(PD_LEVEL_FROM_LSB, addr)
+}
+
+//
+
+#[repr(C, align(4096))]
+struct TranslationStructure {
+    entries: [u64; 512],
+}
+
 #[used]
 #[no_mangle]
 static mut boot_lvl0_upper: TranslationStructure = TranslationStructure { entries: [0; 512] };
@@ -24,56 +66,42 @@ pub fn init_translation_structures(kernel_phys_start: usize, kernel_virt_start: 
         let boot_lvl1_upper_addr = &boot_lvl1_upper as *const TranslationStructure as u64;
         let boot_lvl2_upper_addr = &boot_lvl2_upper as *const TranslationStructure as u64;
 
-        boot_lvl0_lower.entries[0] = boot_lvl1_lower_addr | bit(1) | bit(0);
+        boot_lvl0_lower.entries[0] = 0
+            | boot_lvl1_lower_addr
+            | bit(1) | bit(0) // valid
+        ;
 
         for i in 0..512 {
-            boot_lvl1_lower.entries[i] =
-                ((i as u64) << ARM_1GB_BLOCK_BITS) | bit(10) | (0u64 << 2) | bit(0);
+            boot_lvl1_lower.entries[i] = 0
+                | ((i as u64) << ARM_1GB_BLOCK_BITS)
+                | bit(10) // access flag
+                | (0 << 2) // select MT_DEVICE_nGnRnE
+                | bit(0) // mark as valid
+            ;
         }
 
-        boot_lvl0_lower.entries[get_pgd_index(kernel_virt_start as u64) as usize] =
-            boot_lvl1_upper_addr | bit(1) | bit(0);
-        boot_lvl1_upper.entries[get_pud_index(kernel_virt_start as u64) as usize] =
-            boot_lvl2_upper_addr | bit(1) | bit(0);
+        boot_lvl0_lower.entries[get_pgd_index(kernel_virt_start as u64) as usize] = 0
+            | boot_lvl1_upper_addr
+            | bit(1) | bit(0) // mark as valid
+        ;
 
-        let pmd_index = get_pmd_index(kernel_virt_start as u64);
-        for i in (pmd_index as usize)..512 {
-            boot_lvl2_upper.entries[i] = ((((i as u64) - pmd_index) << ARM_2MB_BLOCK_BITS) + (kernel_phys_start as u64))
-                    | bit(10) /* access flag */
+        boot_lvl1_upper.entries[get_pud_index(kernel_virt_start as u64) as usize] = 0
+            | boot_lvl2_upper_addr
+            | bit(1) | bit(0) // mark as valid
+        ;
 
-                    | (4 << 2) /* MT_NORMAL memory */
-                    | bit(0); /* 2M block */
-            sel4_config::sel4_cfg_if! {
-                if #[cfg(not(MAX_NUM_NODES = "1"))] {
-                    boot_lvl2_upper.entries[i] |= 3 << 8;
-                }
+        let pd_index = get_pd_index(kernel_virt_start as u64) as usize;
+        for i in pd_index..512 {
+            let mut entry = 0
+                | ((((i - pd_index) as u64) << ARM_2MB_BLOCK_BITS) + (kernel_phys_start as u64))
+                | bit(10) // access flag
+                | (4 << 2) // select MT_NORMAL
+                | bit(0) // mark as valid
+            ;
+            if sel4_config::sel4_cfg_usize!(MAX_NUM_NODES) > 1 {
+                entry |= 3 << 8;
             }
+            boot_lvl2_upper.entries[i] = entry;
         }
     }
-}
-
-//
-
-#[repr(C, align(4096))]
-struct TranslationStructure {
-    entries: [u64; 512],
-}
-
-fn bit(i: u64) -> u64 {
-    1 << i
-}
-
-const ARM_1GB_BLOCK_BITS: u64 = 30;
-const ARM_2MB_BLOCK_BITS: u64 = 21;
-
-fn get_pgd_index(addr: u64) -> u64 {
-    (addr >> (12 + 9 + 9 + 9)) & ((1 << 9) - 1)
-}
-
-fn get_pud_index(addr: u64) -> u64 {
-    (addr >> (12 + 9 + 9)) & ((1 << 9) - 1)
-}
-
-fn get_pmd_index(addr: u64) -> u64 {
-    (addr >> (12 + 9)) & ((1 << 9) - 1)
 }
