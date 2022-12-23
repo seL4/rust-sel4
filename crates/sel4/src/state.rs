29cce7ef473c3e0ec409080ca46b1a79ddb793b2
Nick Spinale <nick@nickspinale.com>
2022-12-23 22:46:15 +0000

    Remove explicit "tls" feature

--- crates/sel4/src/state.rs
+++ crates/sel4/src/state.rs
@@ -5,11 +5,11 @@ use crate::{IPCBuffer, InvocationContext};
 const IPC_BUFFER_INIT: RefCell<Option<IPCBuffer>> = RefCell::new(None);
 
 cfg_if::cfg_if! {
-    if #[cfg(feature = "tls")] {
+    if #[cfg(not(feature = "single-threaded"))] {
         #[thread_local]
         pub static IPC_BUFFER: RefCell<Option<IPCBuffer>> = IPC_BUFFER_INIT;
-    } else if #[cfg(feature = "single-threaded")] {
-        use core::ops::{Deref, DerefMut};
+    } else {
+        use core::ops::Deref;
 
         pub static IPC_BUFFER: SingleThreaded<RefCell<Option<IPCBuffer>>> = SingleThreaded(IPC_BUFFER_INIT);
 
@@ -24,14 +24,6 @@ cfg_if::cfg_if! {
                 &self.0
             }
         }
-
-        impl<T> DerefMut for SingleThreaded<T> {
-            fn deref_mut(&mut self) -> &mut Self::Target {
-                &mut self.0
-            }
-        }
-    } else {
-        compile_error!("The feature \"state\" requires at least one of \"tls\" or \"single-threaded\"");
     }
 }
 
