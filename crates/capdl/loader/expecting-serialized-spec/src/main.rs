591fdaf4e59ac6b232de6b563ec3a9a8493fb2df
Nick Spinale <nick@nickspinale.com>
2023-01-31 08:39:10 +0000

    Add empty capdl-loader-expecting-serialized-spec

---
+++ crates/capdl/loader/expecting-serialized-spec/src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+#![feature(const_trait_impl)]
+
+use capdl_loader_core::{load, LoaderBuffers};
+use sel4::BootInfo;
+use sel4_logging::{LevelFilter, Logger, LoggerBuilder};
+
+const LOG_LEVEL: LevelFilter = LevelFilter::Info;
+
+static LOGGER: Logger = LoggerBuilder::default()
+    .level_filter(LOG_LEVEL)
+    .write(|s| sel4::debug_print!("{}", s))
+    .build();
+
+// static mut BUFFERS: LoaderBuffers<{ SPEC.objects.as_slice().len() }> = LoaderBuffers::new();
+
+#[sel4_full_root_task_runtime::main]
+fn main(bootinfo: &BootInfo) -> ! {
+    LOGGER.set().unwrap();
+    // load(&SPEC, &bootinfo, unsafe { &mut BUFFERS }).unwrap_or_else(|err| panic!("Error: {}", err))
+    todo!()
+}
