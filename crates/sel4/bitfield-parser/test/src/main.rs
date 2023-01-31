e3bbadfea6fb99d93daaceb188e493adae2e8c66
Nick Spinale <nick@nickspinale.com>
2023-01-31 01:00:34 +0000

    Add bitfield parser test

---
+++ crates/sel4/bitfield-parser/test/src/main.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use std::path::Path;
+
+use clap::{App, Arg, ArgAction};
+use glob::glob;
+
+fn main() {
+    let matches = App::new("")
+        .arg(
+            Arg::new("dir")
+                .short('d')
+                .required(true)
+                .action(ArgAction::Append),
+        )
+        .get_matches();
+    let dirs = matches.get_many::<String>("dir").unwrap();
+    for d in dirs {
+        for f in glob(&format!("{}/**/*.pbf", d))
+            .unwrap()
+            .map(Result::unwrap)
+        {
+            test_on_path(f);
+        }
+    }
+}
+
+fn test_on_path(f: impl AsRef<Path>) {
+    println!("parsing '{}'", f.as_ref().display());
+    let text = fs::read_to_string(f).unwrap();
+    let file = sel4_bitfield_parser::parse(&text);
+    println!("{:#?}", file);
+}
