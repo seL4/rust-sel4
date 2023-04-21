cac38480d3d35fd21e9b6660a8edeb7c55b98bb7
Nick Spinale <nick@nickspinale.com>
2023-04-21 10:17:48 +0100

    nix: Refactor for clarity

--- hacking/nix/default.nix
+++ hacking/nix/default.nix
@@ -19,47 +19,7 @@ in
 
 let
 
-  treeHelpers = rec {
-
-    mkLeaf = value: {
-      __leaf = null;
-      inherit value;
-    };
-
-    untree = lib.mapAttrs (k: v:
-      if lib.isAttrs v
-      then (
-        if v ? __leaf
-        then v.value
-        else untree v
-      )
-      else v
-    );
-
-    mapLeaves = f: lib.mapAttrs (k: v:
-      if lib.isAttrs v
-      then (
-        if v ? __leaf
-        then mkLeaf (f v.value)
-        else mapLeaves f v
-      )
-      else v
-    );
-
-    leaves =
-      let
-        f = acc: v:
-          if lib.isAttrs v
-          then lib.mapAttrsToList (k': f (acc ++ [k']))
-          else acc
-        ;
-      in
-        f [];
-  };
-
-in
-
-let
+  treeHelpers = import ./tree-helpers.nix { inherit lib; };
 
   makeOverridableWith = f: g: x: (g x) // {
     override = x': makeOverridableWith f g (f x' x);
---
+++ hacking/nix/tree-helpers.nix
@@ -0,0 +1,38 @@
+{ lib }:
+
+rec {
+  mkLeaf = value: {
+    __leaf = null;
+    inherit value;
+  };
+
+  untree = lib.mapAttrs (k: v:
+    if lib.isAttrs v
+    then (
+      if v ? __leaf
+      then v.value
+      else untree v
+    )
+    else v
+  );
+
+  mapLeaves = f: lib.mapAttrs (k: v:
+    if lib.isAttrs v
+    then (
+      if v ? __leaf
+      then mkLeaf (f v.value)
+      else mapLeaves f v
+    )
+    else v
+  );
+
+  leaves =
+    let
+      f = acc: v:
+        if lib.isAttrs v
+        then lib.mapAttrsToList (k': f (acc ++ [k']))
+        else acc
+      ;
+    in
+      f [];
+}
