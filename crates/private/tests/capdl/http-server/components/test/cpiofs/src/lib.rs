266f747b0aff2498ecaca51d3ce2f98e41e0cf07
Nick Spinale <nick@nickspinale.com>
2023-06-29 01:23:08 +0100

    Improve concurrency in HTTP server test

--- crates/private/tests/capdl/http-server/components/test/cpiofs/src/lib.rs
+++ crates/private/tests/capdl/http-server/components/test/cpiofs/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![feature(async_fn_in_trait)]
 #![feature(int_roundings)]
+#![feature(slice_as_chunks)]
 
 extern crate alloc;
 
@@ -12,6 +13,7 @@ use core::marker::PhantomData;
 use core::mem;
 use core::num::NonZeroUsize;
 
+use futures::future;
 use hex::FromHex;
 use lru::LruCache;
 use zerocopy::{AsBytes, FromBytes};
@@ -226,20 +228,51 @@ impl<T, const BLOCK_SIZE: usize> BlockIOAdapter<T, BLOCK_SIZE> {
     }
 }
 
+impl<const BLOCK_SIZE: usize, T: BlockIO<BLOCK_SIZE>> BlockIOAdapter<T, BLOCK_SIZE> {
+    async fn read_partial_block(&self, block_id: usize, offset_into_block: usize, buf: &mut [u8]) {
+        assert!(offset_into_block + buf.len() <= BLOCK_SIZE);
+        let mut block_buf = [0; BLOCK_SIZE];
+        self.inner().read_block(block_id, &mut block_buf).await;
+        buf.copy_from_slice(&block_buf[offset_into_block..][..buf.len()]);
+    }
+}
+
 impl<const BLOCK_SIZE: usize, T: BlockIO<BLOCK_SIZE>> IO for BlockIOAdapter<T, BLOCK_SIZE> {
     async fn read(&self, offset: usize, buf: &mut [u8]) {
-        let mut block_buf = [0; BLOCK_SIZE];
-        let start_offset = offset;
-        let end_offset = offset + buf.len();
-        let start_block_id = start_offset / BLOCK_SIZE;
-        let end_block_id = end_offset.next_multiple_of(BLOCK_SIZE) / BLOCK_SIZE;
-        for block_id in start_block_id..end_block_id {
-            self.inner.read_block(block_id, &mut block_buf).await;
-            let this_start_offset = start_offset.max(block_id * BLOCK_SIZE);
-            let this_end_offset = end_offset.min((block_id + 1) * BLOCK_SIZE);
-            let this_len = this_end_offset - this_start_offset;
-            buf[this_start_offset - start_offset..this_end_offset - start_offset]
-                .copy_from_slice(&block_buf[this_start_offset % BLOCK_SIZE..][..this_len]);
+        let offset_of_first_full_chunk = offset.next_multiple_of(BLOCK_SIZE);
+        let block_id_of_first_full_chunk = offset_of_first_full_chunk / BLOCK_SIZE;
+        if offset_of_first_full_chunk > offset + buf.len() {
+            let block_id = block_id_of_first_full_chunk - 1;
+            let offset_into_block = offset - block_id * BLOCK_SIZE;
+            self.read_partial_block(block_id, offset_into_block, buf)
+                .await;
+        } else {
+            let (left_partial_chunk, rest) = buf.split_at_mut(offset_of_first_full_chunk - offset);
+            let (mid_chunks, right_partial_chunk) = rest.as_chunks_mut::<BLOCK_SIZE>();
+            let num_mid_chunks = mid_chunks.len();
+            future::join3(
+                future::join_all(mid_chunks.iter_mut().enumerate().map(|(i, chunk)| {
+                    let block_id = block_id_of_first_full_chunk + i;
+                    self.inner().read_block(block_id, chunk)
+                })),
+                async {
+                    if left_partial_chunk.len() > 0 {
+                        let block_id = block_id_of_first_full_chunk - 1;
+                        let offset_into_block = BLOCK_SIZE - left_partial_chunk.len();
+                        self.read_partial_block(block_id, offset_into_block, left_partial_chunk)
+                            .await;
+                    }
+                },
+                async {
+                    if right_partial_chunk.len() > 0 {
+                        let block_id = block_id_of_first_full_chunk + num_mid_chunks;
+                        let offset_into_block = 0;
+                        self.read_partial_block(block_id, offset_into_block, right_partial_chunk)
+                            .await;
+                    }
+                },
+            )
+            .await;
         }
     }
 }
@@ -277,7 +310,7 @@ impl<T: BlockIO<BLOCK_SIZE>, const BLOCK_SIZE: usize> BlockIO<BLOCK_SIZE>
     for CachedBlockIO<T, BLOCK_SIZE>
 {
     async fn read_block(&self, block_id: usize, buf: &mut [u8; BLOCK_SIZE]) {
-        // odd control flow to avoid holding core::cell::RefMut across await
+        // NOTE: odd control flow to avoid holding core::cell::RefMut across await
         if let Some(block) = self.lru.borrow_mut().get(&block_id) {
             *buf = block.clone();
             return;
--- hacking/cargo-manifest-sources/crates/private/tests/capdl/http-server/components/test/cpiofs/crate.nix
+++ hacking/cargo-manifest-sources/crates/private/tests/capdl/http-server/components/test/cpiofs/crate.nix
@@ -6,5 +6,12 @@ mk {
     inherit (versions) log zerocopy;
     hex = { version = "0.4.3"; default-features = false; };
     lru = "0.10.0";
+    futures = {
+      version = versions.futures;
+      default-features = false;
+      features = [
+        "alloc"
+      ];
+    };
   };
 }
