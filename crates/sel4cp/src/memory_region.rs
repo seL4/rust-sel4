92a473a85d0ee911bc5725c09f471138046dfb27
Nick Spinale <nick@nickspinale.com>
2023-07-11 22:08:43 +0100

    crates/sel4cp: Add support for attributes in memory_region!() macro

--- crates/sel4cp/src/memory_region.rs
+++ crates/sel4cp/src/memory_region.rs
@@ -11,35 +11,72 @@ use core::ptr::NonNull;
 /// ```rust
 /// #[macro_export]
 /// macro_rules! memory_region_symbol {
-///     ($symbol:ident: *mut [$ty:ty], n = $n:expr) => {{
-///         core::ptr::NonNull::slice_from_raw_parts(
-///             $crate::memory_region_symbol!($symbol: *mut $ty),
-///             $n,
-///         )
-///     }};
-///     ($symbol:ident: *mut $ty:ty) => {{
-///         #[no_mangle]
-///         #[link_section = ".data"]
-///         static mut $symbol: *mut $ty = core::ptr::null_mut();
+///    ($(#[$attrs:meta])* $symbol:ident: *mut [$ty:ty], n = $n:expr) => {{
+///        core::ptr::NonNull::slice_from_raw_parts(
+///            $crate::memory_region_symbol!($(#[$attrs])* $symbol: *mut $ty),
+///            $n,
+///        )
+///    }};
+///    ($(#[$attrs:meta])* $symbol:ident: *mut $ty:ty) => {{
+///        $(#[$attrs])*
+///        #[no_mangle]
+///        #[link_section = ".data"]
+///        static mut $symbol: *mut $ty = core::ptr::null_mut();
 ///
-///         core::ptr::NonNull::new($symbol).unwrap_or_else(|| {
-///             panic!("{} is null", stringify!($symbol))
-///         })
-///     }};
+///        core::ptr::NonNull::new($symbol).unwrap_or_else(|| {
+///            panic!("{} is null", stringify!($symbol))
+///        })
+///    }};
 /// }
 /// ```
 ///
 /// The patching mechanism used by the `sel4cp` tool requires that the symbol be allocated space in
 /// the protection domain's ELF file, so we delare the symbol as part of the `.data` section.
+///
+/// # Examples
+///
+/// ```rust
+/// let region_1 = unsafe {
+///     ExternallySharedRef::<'static, Foo>::new(
+///         memory_region_symbol!(region_1_addr: *mut Foo),
+///     )
+/// };
+///
+/// let region_2 = unsafe {
+///     ExternallySharedRef::<'static, [u8]>::new_read_only(
+///         memory_region_symbol!(region_2_addr: *mut [u8], n = REGION_2_SIZE),
+///     )
+/// };
+/// ```
+///
+/// # Note
+///
+/// The `sel4cp` tool requires memory region address symbols to be present in protection domain
+/// binaries. To prevent Rust from optimizing them out in cases where it is not used, add the
+/// unstable `#[used(linker)]` attribute. For example:
+///
+/// ```rust
+/// #![feature(used_with_arg)]
+///
+/// // might be optimized away if not used
+/// memory_region_symbol!(region_addr: *mut Foo)
+///
+/// // won't be optimized away
+/// memory_region_symbol! {
+///    #[used(linker)]
+///    region_addr: *mut Foo
+/// }
+/// ```
 #[macro_export]
 macro_rules! memory_region_symbol {
-    ($symbol:ident: *mut [$ty:ty], n = $n:expr) => {{
+    ($(#[$attrs:meta])* $symbol:ident: *mut [$ty:ty], n = $n:expr) => {{
         core::ptr::NonNull::slice_from_raw_parts(
-            $crate::memory_region_symbol!($symbol: *mut $ty),
+            $crate::memory_region_symbol!($(#[$attrs])* $symbol: *mut $ty),
             $n,
         )
     }};
-    ($symbol:ident: *mut $ty:ty) => {{
+    ($(#[$attrs:meta])* $symbol:ident: *mut $ty:ty) => {{
+        $(#[$attrs])*
         #[no_mangle]
         #[link_section = ".data"]
         static mut $symbol: *mut $ty = core::ptr::null_mut();
