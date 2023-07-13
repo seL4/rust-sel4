8d6fc0dc402bd1290d81375ce9f8a5a3f7580736
Nick Spinale <nick@nickspinale.com>
2023-07-13 23:54:24 +0100

    Specify RUST_TARGET_PATH in .cargo/config

--- hacking/nix/scope/world/shell.nix
+++ hacking/nix/scope/world/shell.nix
@@ -14,7 +14,6 @@
 let
   kernelLoaderConfigEnvVars = lib.optionalAttrs (!worldConfig.isCorePlatform && worldConfig.kernelLoaderConfig != null) {
     SEL4_KERNEL_LOADER_CONFIG = writeText "loader-config.json" (builtins.toJSON worldConfig.kernelLoaderConfig);
-    SEL4_APP = "${seL4ForBoot}/bin/kernel.elf"; # HACK
   };
 
   capdlEnvVars = lib.optionalAttrs (!worldConfig.isCorePlatform) {
@@ -24,18 +23,14 @@ let
 
 in
 mkShell (seL4RustEnvVars // kernelLoaderConfigEnvVars // capdlEnvVars // {
-  RUST_TARGET_PATH = toString (sources.srcRoot + "/support/targets");
-
   # TODO
   RUST_SEL4_TARGET = defaultRustTargetInfo.name;
-  # RUST_SEL4_TARGET = "aarch64-sel4cp";
 
   RUST_BARE_METAL_TARGET = bareMetalRustTargetInfo.name;
 
   HOST_CARGO_FLAGS = lib.concatStringsSep " " [
     "-Z" "build-std=core,alloc,compiler_builtins"
     "-Z" "build-std-features=compiler-builtins-mem"
-    # "--target" RUST_SEL4_TARGET
   ];
 
   LIBCLANG_PATH = "${lib.getLib buildPackages.llvmPackages.libclang}/lib";
