de62ccb2096ac4648ec781a096d996e4ee89c963
Nick Spinale <nick@nickspinale.com>
2023-08-02 16:43:47 +0100

    Improve profile handling in Docker container

--- hacking/docker/setup.sh
+++ hacking/docker/setup.sh
@@ -3,5 +3,7 @@ set -e
 if [ ! -f /nix/.installed ]; then
     curl -L https://nixos.org/nix/install | \
         sh -s -- --yes --no-channel-add --no-modify-profile
+    ln -s $(readlink --canonicalize $HOME/.nix-profile) /nix/var/nix/the-profile
+    rm -r $HOME/.nix-profile $HOME/.nix-defexpr $HOME/.local/state/nix
     touch /nix/.installed
 fi
