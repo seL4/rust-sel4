887499ae2f7b6d586267cf4014fcdfd28b119c15
Nick Spinale <nick@nickspinale.com>
2023-09-05 09:31:20 +0000

    crates/private/tests/capdl/threads: Distribute threads across cores

--- crates/private/tests/capdl/threads/cdl.py
+++ crates/private/tests/capdl/threads/cdl.py
@@ -6,9 +6,11 @@ class TestComponent(ElfComponent):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        max_affinity = int(self.composition.kernel_config['MAX_NUM_NODES']) - 1
+
         self.primary_thread.tcb['sc_slot'] = self.new_sched_context("primary")
 
-        secondary_thread = self.secondary_thread('secondary_thread', sched_context=self.new_sched_context("secondary"))
+        secondary_thread = self.secondary_thread('secondary_thread', affinity=max_affinity, sched_context=self.new_sched_context("secondary"))
         lock_nfn = self.alloc(ObjectType.seL4_NotificationObject, name='lock_nfn')
         barrier_nfn = self.alloc(ObjectType.seL4_NotificationObject, name='barrier_nfn')
 
