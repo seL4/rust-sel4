c51bf6be8837b6a98c41f58430fd98a0a2d9540b
Nick Spinale <nick@nickspinale.com>
2023-09-22 08:37:34 +0000

    crates/sel4/config: Don't use C libsel4 config
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4/config/data/build.rs
+++ crates/sel4/config/data/build.rs
@@ -1,25 +1,15 @@
 use std::env;
 use std::fs::File;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use sel4_build_env::find_in_libsel4_include_dirs;
 use sel4_config_generic_types::Configuration;
 
 fn main() {
-    let config = {
-        let kernel_config = from_path(find_in_libsel4_include_dirs("kernel/gen_config.json"));
-        let libsel4_config = from_path(find_in_libsel4_include_dirs("sel4/gen_config.json"));
-        let mut this = Configuration::empty();
-        this.append(kernel_config);
-        this.append(libsel4_config);
-        this
-    };
-
+    let config_json_path = find_in_libsel4_include_dirs("kernel/gen_config.json");
+    let config =
+        Configuration::new(serde_json::from_reader(File::open(config_json_path).unwrap()).unwrap());
     let out_dir = env::var("OUT_DIR").unwrap();
     let out_path = PathBuf::from(&out_dir).join("kernel_config.json");
     serde_json::to_writer_pretty(File::create(out_path).unwrap(), &config).unwrap()
-}
-
-fn from_path(path: impl AsRef<Path>) -> Configuration {
-    serde_json::from_reader(File::open(path).unwrap()).unwrap()
 }
