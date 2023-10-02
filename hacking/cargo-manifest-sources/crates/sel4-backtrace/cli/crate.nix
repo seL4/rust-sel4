8a25daae0e80fda2384dabe47acb583cbb80924d
Nick Spinale <nick@nickspinale.com>
2023-10-02 09:46:12 +0000

    crates/sel4-backtrace/cli: Remove mmap crate dependency
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- hacking/cargo-manifest-sources/crates/sel4-backtrace/cli/crate.nix
+++ hacking/cargo-manifest-sources/crates/sel4-backtrace/cli/crate.nix
@@ -4,7 +4,6 @@ mk {
   package.name = "sel4-backtrace-cli";
   dependencies = {
     sel4-backtrace-types.features = [ "full" ];
-    memmap = "0.7.0";
     hex = "0.4.3";
     inherit (versions) object addr2line clap;
   };
