415e5a0b395c53b72c680f3d28bfb3a718e494d2
Nick Spinale <nick@nickspinale.com>
2023-10-10 07:14:35 +0000

    crates/sel4-async/timers: Rework
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- hacking/cargo-manifest-sources/crates/sel4-async/timers/crate.nix
+++ hacking/cargo-manifest-sources/crates/sel4-async/timers/crate.nix
@@ -3,14 +3,13 @@
 mk {
   package.name = "sel4-async-timers";
   dependencies = {
-    inherit (versions) log;
-    futures = {
-      version = versions.futures;
-      default-features = false;
-      features = [
-        "alloc"
-      ];
-    };
-    smoltcp = smoltcpWith [];
+    inherit (versions) log pin-project;
+    # futures = {
+    #   version = versions.futures;
+    #   default-features = false;
+    #   features = [
+    #     "alloc"
+    #   ];
+    # };
   };
 }
