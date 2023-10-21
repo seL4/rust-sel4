88b1c1bbe2e791614a2fe12e1bd575c171686c45
Nick Spinale <nick@nickspinale.com>
2023-10-21 11:33:38 +0000

    Add licensing information to Nix sources
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- hacking/nix/scope/world/docs.nix
+++ hacking/nix/scope/world/docs.nix
@@ -1,3 +1,9 @@
+#
+# Copyright 2023, Colias Group, LLC
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
 { lib, buildPlatform, hostPlatform, buildPackages
 , runCommandCC, linkFarm, symlinkJoin, emptyDirectory
 , rsync
--- hacking/nix/scope/world/instances/microkit/banscii/default.nix
+++ hacking/nix/scope/world/instances/microkit/banscii/default.nix
@@ -1,3 +1,9 @@
+#
+# Copyright 2023, Colias Group, LLC
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
 { lib, stdenv
 , buildPackages, pkgsBuildBuild
 , linkFarm, symlinkJoin, writeText, writeScript, runCommand
