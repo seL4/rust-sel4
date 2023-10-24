9acd9177c256266f0e2eb2d60ef80b2e7c81f01a
Nick Spinale <nick@nickspinale.com>
2023-10-24 01:16:21 +0000

    crates/sel4-externally-shared: Fix licensing information
    As part of more a detailed pass.
    
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- hacking/cargo-manifest-sources/crates/sel4-externally-shared/crate.nix
+++ hacking/cargo-manifest-sources/crates/sel4-externally-shared/crate.nix
@@ -1,13 +1,15 @@
 #
 # Copyright 2023, Colias Group, LLC
 #
-# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-License-Identifier: MIT OR Apache-2.0
 #
 
 { mk, localCrates, versions, volatileSource }:
 
-mk {
+mk rec {
   package.name = "sel4-externally-shared";
+  package.license = "MIT OR Apache-2.0";
+  nix.reuseFrontmatterArgs.licenseID = package.license;
   nix.local.dependencies = with localCrates; [
     # volatile
   ];
