249c3f15b0e68b0d6a760831c138d8cc9b0702a2
Nick Spinale <nick@nickspinale.com>
2023-10-24 11:22:29 +0000

    Simplify Cargo manifest generation Nix tool
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- hacking/cargo-manifest-management/manual-manifests.nix
+++ hacking/cargo-manifest-management/manual-manifests.nix
@@ -4,11 +4,15 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-{
-  # "virtio-drivers" = "tmp/virtio-drivers";
-  # "mbedtls" = "tmp/rust-mbedtls/mbedtls";
-  # "mbedtls-sys-auto" = "tmp/rust-mbedtls/mbedtls-sys";
-  # "mbedtls-platform-support" = "tmp/rust-mbedtls/mbedtls-platform-support";
-  # "embedded-fat" = "tmp/rust-embedded-fat";
-  # "volatile" = "tmp/volatile";
+let
+
+  relativeToWorkspaceRoot = relativePath: "${toString ../..}/${relativePath}";
+
+in {
+  # "virtio-drivers" = relativeToWorkspaceRoot "tmp/virtio-drivers";
+  # "mbedtls" = relativeToWorkspaceRoot "tmp/rust-mbedtls/mbedtls";
+  # "mbedtls-sys-auto" = relativeToWorkspaceRoot "tmp/rust-mbedtls/mbedtls-sys";
+  # "mbedtls-platform-support" = relativeToWorkspaceRoot "tmp/rust-mbedtls/mbedtls-platform-support";
+  # "embedded-fat" = relativeToWorkspaceRoot "tmp/rust-embedded-fat";
+  # "volatile" = relativeToWorkspaceRoot "tmp/volatile";
 }
