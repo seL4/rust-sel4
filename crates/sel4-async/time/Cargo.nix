3d5790a03befca1d92975d8e8e9c750d0edda228
Nick Spinale <nick@nickspinale.com>
2024-01-08 21:19:48 +0000

    crates/sel4-async/time: Clean up Cargo.nix
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-async/time/Cargo.nix
+++ crates/sel4-async/time/Cargo.nix
@@ -10,12 +10,5 @@ mk {
   package.name = "sel4-async-time";
   dependencies = {
     inherit (versions) log pin-project;
-    # futures = {
-    #   version = versions.futures;
-    #   default-features = false;
-    #   features = [
-    #     "alloc"
-    #   ];
-    # };
   };
 }
