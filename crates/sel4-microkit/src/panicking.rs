983b3a1c88b945cff65a4e0c211e32af8ad78884
Nick Spinale <nick@nickspinale.com>
2024-01-09 08:19:31 +0000

    Remove unstable macro features from sel4-microkit crate
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-microkit/src/panicking.rs
+++ crates/sel4-microkit/src/panicking.rs
@@ -43,8 +43,9 @@ pub(crate) fn init_panicking() {
 #[no_mangle]
 #[allow(unused_variables)]
 fn sel4_runtime_debug_put_char(c: u8) {
-    #[sel4::sel4_cfg(PRINTING)]
-    {
-        sel4::debug_put_char(c as core::ffi::c_char)
+    sel4::sel4_cfg_if! {
+        if #[cfg(PRINTING)] {
+            sel4::debug_put_char(c as core::ffi::c_char)
+        }
     }
 }
