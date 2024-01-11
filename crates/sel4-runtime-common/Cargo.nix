c1ee6a51740e6c10ea9f4900a37df9547e931f0b
Nick Spinale <nick@nickspinale.com>
2024-01-11 23:07:25 +0000

    Rework TLS initialization
    Remove 'sel4-initialize-tls-on-stack's dependency on 'sel4'
    
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-runtime-common/Cargo.nix
+++ crates/sel4-runtime-common/Cargo.nix
@@ -11,12 +11,14 @@ mk {
   dependencies = {
     inherit (versions) cfg-if;
     unwinding = unwindingWith [] // { optional = true; };
+    inherit (localCrates) sel4-panicking-env;
+    sel4 = localCrates.sel4 // { default-features = false; optional = true; };
     sel4-initialize-tls-on-stack = localCrates.sel4-initialize-tls-on-stack // { optional = true; };
     sel4-sync = localCrates.sel4-sync // { optional = true; };
     sel4-dlmalloc = localCrates.sel4-dlmalloc // { optional = true; };
   };
   features = {
-    tls = [ "dep:sel4-initialize-tls-on-stack" ];
+    tls = [ "dep:sel4-initialize-tls-on-stack" "dep:sel4" ];
     start = [];
     static-heap = [ "sel4-sync" "sel4-dlmalloc" ];
   };
