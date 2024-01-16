3d096cdc83cd2ed24b92bb6373bbc980e54f53ea
Nick Spinale <nick@nickspinale.com>
2024-01-16 10:39:25 +0000

    crates/sel4-runtime-common: Remove heap module
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-runtime-common/Cargo.nix
+++ crates/sel4-runtime-common/Cargo.nix
@@ -14,12 +14,9 @@ mk {
     inherit (localCrates) sel4-panicking-env;
     sel4 = localCrates.sel4 // { default-features = false; optional = true; };
     sel4-initialize-tls-on-stack = localCrates.sel4-initialize-tls-on-stack // { optional = true; };
-    sel4-sync = localCrates.sel4-sync // { optional = true; };
-    sel4-dlmalloc = localCrates.sel4-dlmalloc // { optional = true; };
   };
   features = {
     tls = [ "dep:sel4-initialize-tls-on-stack" "dep:sel4" ];
     start = [];
-    static-heap = [ "sel4-sync" "sel4-dlmalloc" ];
   };
 }
