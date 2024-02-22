88e9d4ad95c0da170841ea648caf98de8690ef7a
Nick Spinale <nick@nickspinale.com>
2024-02-22 23:23:50 +0000

    Add spawn-thread example
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

---
+++ crates/examples/root-task/spawn-thread/Cargo.nix
@@ -0,0 +1,20 @@
+#
+# Copyright 2024, Colias Group, LLC
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+{ mk, versions, localCrates }:
+
+mk {
+  package.name = "spawn-thread";
+  dependencies = {
+    inherit (versions) cfg-if;
+    inherit (localCrates)
+      sel4
+      sel4-root-task
+      sel4-elf-header
+      sel4-initialize-tls
+    ;
+  };
+}
