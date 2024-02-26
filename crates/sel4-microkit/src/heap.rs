e02e6389497661e8160777947686f47bbe20278b
Nick Spinale <nick@nickspinale.com>
2024-02-26 23:11:02 +0000

    crates/sel4-microkit: Refactor heap
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-microkit/src/heap.rs
+++ crates/sel4-microkit/src/heap.rs
@@ -4,37 +4,32 @@
 // SPDX-License-Identifier: BSD-2-Clause
 //
 
-use sel4_dlmalloc::StaticDlmallocGlobalAlloc;
-use sel4_sync::{GenericRawMutex, PanickingMutexSyncOps};
-
-pub use sel4_dlmalloc::StaticHeap;
-
-#[doc(hidden)]
-pub type GlobalAllocator<const N: usize> =
-    StaticDlmallocGlobalAlloc<GenericRawMutex<PanickingMutexSyncOps>, &'static StaticHeap<N>>;
-
-#[doc(hidden)]
-pub const fn new_global_allocator<const N: usize>(
-    bounds: &'static StaticHeap<N>,
-) -> GlobalAllocator<N> {
-    StaticDlmallocGlobalAlloc::new(GenericRawMutex::new(PanickingMutexSyncOps::new()), bounds)
-}
-
 #[doc(hidden)]
 #[macro_export]
 macro_rules! declare_heap {
     ($size:expr) => {
         const _: () = {
-            #[global_allocator]
-            static GLOBAL_ALLOCATOR: $crate::_private::heap::GlobalAllocator<{ $size }> = {
-                static STATIC_HEAP: $crate::_private::heap::StaticHeap<{ $size }> =
-                    $crate::_private::heap::StaticHeap::new();
-                $crate::_private::heap::new_global_allocator(&STATIC_HEAP)
-            };
+            mod _x {
+                use $crate::_private::heap::*;
+
+                static STATIC_HEAP: StaticHeap<{ $size }> = StaticHeap::new();
+
+                #[global_allocator]
+                static GLOBAL_ALLOCATOR: StaticDlmallocGlobalAlloc<
+                    GenericRawMutex<PanickingMutexSyncOps>,
+                    &'static StaticHeap<{ $size }>,
+                > = {
+                    StaticDlmallocGlobalAlloc::new(
+                        GenericRawMutex::new(PanickingMutexSyncOps::new()),
+                        &STATIC_HEAP,
+                    )
+                };
+            }
         };
     };
 }
 
 pub mod _private {
-    pub use super::{new_global_allocator, GlobalAllocator, StaticHeap};
+    pub use sel4_dlmalloc::{StaticDlmallocGlobalAlloc, StaticHeap};
+    pub use sel4_sync::{GenericRawMutex, PanickingMutexSyncOps};
 }
