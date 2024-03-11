c8fd0dd6573a2f07273f14d0a9cb6b34cc7426f6
Nick Spinale <nick@nickspinale.com>
2024-03-11 08:23:13 +0000

    crates/sel4: Organize x86 modules
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4/src/arch/x86/arch/x64/mod.rs
+++ crates/sel4/src/arch/x86/arch/x64/mod.rs
@@ -4,15 +4,12 @@
 // SPDX-License-Identifier: MIT
 //
 
-mod invocations;
 mod object;
 mod user_context;
-mod vspace;
 
 pub(crate) mod top_level {
     pub use super::{
         object::{ObjectBlueprintSeL4Arch, ObjectBlueprintX64, ObjectTypeSeL4Arch, ObjectTypeX64},
         user_context::UserContext,
-        vspace::{FrameObjectType, TranslationStructureObjectType},
     };
 }
