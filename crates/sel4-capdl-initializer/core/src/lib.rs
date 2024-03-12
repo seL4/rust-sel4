21386ce696af6d95c3e9bf6701027fcfdbddc1ff
Nick Spinale <nick@nickspinale.com>
2024-03-12 08:59:47 +0000

    crates/sel4: Add TranslationStructureObjectType::step_bits
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-capdl-initializer/core/src/lib.rs
+++ crates/sel4-capdl-initializer/core/src/lib.rs
@@ -560,7 +560,7 @@ impl<'a, N: ObjectName, D: Content, M: GetEmbeddedFrame, B: BorrowMut<[PerObject
         obj: &object::PageTable,
     ) -> Result<()> {
         for (i, entry) in obj.entries() {
-            let vaddr = vaddr + (i << sel4::TranslationStructureObjectType::span_bits(level + 1));
+            let vaddr = vaddr + (i << sel4::TranslationStructureObjectType::step_bits(level));
             match entry {
                 PageTableEntry::Frame(cap) => {
                     let frame = self.orig_cap::<cap_type::UnspecifiedFrame>(cap.object);
