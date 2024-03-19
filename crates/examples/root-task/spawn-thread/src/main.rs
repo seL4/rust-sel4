11f1bbadca8303a247a28cd970a5d50b11453b59
Nick Spinale <nick@nickspinale.com>
2024-03-19 08:38:06 +0000

    Simlify code in examples
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/examples/root-task/spawn-thread/src/main.rs
+++ crates/examples/root-task/spawn-thread/src/main.rs
@@ -293,28 +293,13 @@ fn get_user_image_frame_slot(
     bootinfo: &sel4::BootInfo,
     addr: usize,
 ) -> sel4::init_thread::Slot<sel4::cap_type::Granule> {
-    assert_eq!(addr % GRANULE_SIZE, 0);
-    let user_image_footprint = get_user_image_footprint();
-    let num_user_frames = bootinfo.user_image_frames().len();
-    assert_eq!(user_image_footprint.len(), num_user_frames * GRANULE_SIZE);
-    let ix = (addr - user_image_footprint.start) / GRANULE_SIZE;
-    bootinfo.user_image_frames().index(ix)
-}
-
-fn get_user_image_footprint() -> Range<usize> {
     extern "C" {
         static __executable_start: usize;
-        static _end: usize;
     }
-    unsafe {
-        let start = round_down(ptr::addr_of!(__executable_start) as usize, GRANULE_SIZE);
-        let end = (ptr::addr_of!(_end) as usize).next_multiple_of(GRANULE_SIZE);
-        start..end
-    }
-}
-
-const fn round_down(n: usize, b: usize) -> usize {
-    n - n % b
+    let user_image_addr = unsafe { ptr::addr_of!(__executable_start) as usize };
+    bootinfo
+        .user_image_frames()
+        .index(addr / GRANULE_SIZE - user_image_addr / GRANULE_SIZE)
 }
 
 const GRANULE_SIZE: usize = sel4::FrameObjectType::GRANULE.bytes();
