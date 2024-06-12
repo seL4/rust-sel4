a2105f3a20f81d5945466724ca61d45d987f0f9e
Nick Spinale <nick@nickspinale.com>
2024-06-12 06:36:56 +0000

    Set up Banscii for portability
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/examples/microkit/banscii/pds/serial-driver/src/main.rs
+++ crates/examples/microkit/banscii/pds/serial-driver/src/main.rs
@@ -9,6 +9,8 @@
 
 use sel4_microkit::{memory_region_symbol, protection_domain, Channel};
 use sel4_microkit_embedded_hal_adapters::serial::driver::Driver;
+
+#[cfg(feature = "board-qemu_virt_aarch64")]
 use sel4_pl011_driver::Driver as DriverImpl;
 
 const DEVICE: Channel = Channel::new(0);
@@ -17,6 +19,6 @@ const ASSISTANT: Channel = Channel::new(1);
 #[protection_domain]
 fn init() -> Driver<DriverImpl> {
     let driver_impl =
-        unsafe { DriverImpl::new(memory_region_symbol!(pl011_register_block: *mut ()).as_ptr()) };
+        unsafe { DriverImpl::new(memory_region_symbol!(serial_register_block: *mut ()).as_ptr()) };
     Driver::new(driver_impl, DEVICE, ASSISTANT)
 }
