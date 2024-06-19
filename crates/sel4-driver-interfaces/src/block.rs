33fda22f76cfa8256c6585580a4bfbea0948025d
Nick Spinale <nick@nickspinale.com>
2024-06-19 01:10:26 +0000

    Rework GetBlockDeviceLayout trait
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-driver-interfaces/src/block.rs
+++ crates/sel4-driver-interfaces/src/block.rs
@@ -4,9 +4,50 @@
 // SPDX-License-Identifier: BSD-2-Clause
 //
 
-// TODO make fallible?
-pub trait GetBlockLayout {
-    fn get_block_size(&mut self) -> usize;
+use core::cell::RefCell;
+use core::fmt;
+use core::ops::Deref;
 
-    fn get_num_blocks(&mut self) -> u64;
+use lock_api::{Mutex, RawMutex};
+use serde::{Deserialize, Serialize};
+
+use crate::{WrappedMutex, WrappedRefCell, WrappedRefCellError};
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+pub struct MacAddress(pub [u8; 6]);
+
+pub trait GetBlockDeviceLayout {
+    type Error: fmt::Debug;
+
+    fn get_block_size(&mut self) -> Result<usize, Self::Error>;
+
+    fn get_num_blocks(&mut self) -> Result<u64, Self::Error>;
+}
+
+impl<T: Deref<Target = RefCell<U>>, U: GetBlockDeviceLayout> GetBlockDeviceLayout
+    for &WrappedRefCell<T>
+{
+    type Error = WrappedRefCellError<U::Error>;
+
+    fn get_block_size(&mut self) -> Result<usize, Self::Error> {
+        self.with_mut(|this| this.get_block_size())
+    }
+
+    fn get_num_blocks(&mut self) -> Result<u64, Self::Error> {
+        self.with_mut(|this| this.get_num_blocks())
+    }
+}
+
+impl<R: RawMutex, T: Deref<Target = Mutex<R, U>>, U: GetBlockDeviceLayout> GetBlockDeviceLayout
+    for &WrappedMutex<T>
+{
+    type Error = U::Error;
+
+    fn get_block_size(&mut self) -> Result<usize, Self::Error> {
+        self.with_mut(|this| this.get_block_size())
+    }
+
+    fn get_num_blocks(&mut self) -> Result<u64, Self::Error> {
+        self.with_mut(|this| this.get_num_blocks())
+    }
 }
--- crates/sel4-microkit/driver-adapters/src/block/driver.rs
+++ crates/sel4-microkit/driver-adapters/src/block/driver.rs
@@ -4,18 +4,27 @@
 // SPDX-License-Identifier: BSD-2-Clause
 //
 
-use sel4_driver_interfaces::block::GetBlockLayout;
+use sel4_driver_interfaces::block::GetBlockDeviceLayout;
 use sel4_microkit::MessageInfo;
 use sel4_microkit_message::MessageInfoExt as _;
 
 use super::message_types::*;
 
-pub fn handle_client_request<T: GetBlockLayout>(dev: &mut T, msg_info: MessageInfo) -> MessageInfo {
+pub fn handle_client_request<T: GetBlockDeviceLayout>(
+    dev: &mut T,
+    msg_info: MessageInfo,
+) -> MessageInfo {
     match msg_info.recv_using_postcard::<Request>() {
         Ok(req) => {
             let resp: Response = match req {
-                Request::GetNumBlocks => Ok(SuccessResponse::GetNumBlocks(dev.get_num_blocks())),
-                Request::GetBlockSize => Ok(SuccessResponse::GetBlockSize(dev.get_block_size())),
+                Request::GetNumBlocks => dev
+                    .get_num_blocks()
+                    .map(SuccessResponse::GetNumBlocks)
+                    .map_err(|_| ErrorResponse::Unspecified),
+                Request::GetBlockSize => dev
+                    .get_block_size()
+                    .map(SuccessResponse::GetBlockSize)
+                    .map_err(|_| ErrorResponse::Unspecified),
             };
             MessageInfo::send_using_postcard(resp).unwrap()
         }
