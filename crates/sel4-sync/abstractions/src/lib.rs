86a9776e05c11f4c439d1d919991431924119e34
Nick Spinale <nick@nickspinale.com>
2024-06-25 06:36:54 +0000

    crates/sel4-sync/abstractions: Add PanickingRawMutex::new()
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-sync/abstractions/src/lib.rs
+++ crates/sel4-sync/abstractions/src/lib.rs
@@ -14,13 +14,25 @@ pub struct PanickingRawMutex {
     locked: AtomicBool,
 }
 
+impl PanickingRawMutex {
+    pub const fn new() -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+        }
+    }
+}
+
+impl Default for PanickingRawMutex {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 unsafe impl lock_api::RawMutex for PanickingRawMutex {
     type GuardMarker = lock_api::GuardNoSend; // TODO
 
     #[allow(clippy::declare_interior_mutable_const)]
-    const INIT: Self = Self {
-        locked: AtomicBool::new(false),
-    };
+    const INIT: Self = Self::new();
 
     fn lock(&self) {
         if !self.try_lock() {
