8b09c4d1c284b2439c3273868a2da290d2e673bc
Nick Spinale <nick@nickspinale.com>
2024-07-11 20:48:14 +0000

    crates/sel4-sync/abstractions: Add UnsyncPanickingRawMutex
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-sync/abstractions/src/lib.rs
+++ crates/sel4-sync/abstractions/src/lib.rs
@@ -6,6 +6,7 @@
 
 #![no_std]
 
+use core::cell::Cell;
 use core::sync::atomic::{AtomicBool, Ordering};
 
 pub use lock_api;
@@ -47,5 +48,45 @@ unsafe impl lock_api::RawMutex for PanickingRawMutex {
 
     unsafe fn unlock(&self) {
         self.locked.store(false, Ordering::Release)
+    }
+}
+
+pub struct UnsyncPanickingRawMutex {
+    locked: Cell<bool>,
+}
+
+impl UnsyncPanickingRawMutex {
+    pub const fn new() -> Self {
+        Self {
+            locked: Cell::new(false),
+        }
+    }
+}
+
+impl Default for UnsyncPanickingRawMutex {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+unsafe impl lock_api::RawMutex for UnsyncPanickingRawMutex {
+    type GuardMarker = lock_api::GuardNoSend; // TODO
+
+    #[allow(clippy::declare_interior_mutable_const)]
+    const INIT: Self = Self::new();
+
+    fn lock(&self) {
+        if !self.try_lock() {
+            panic!("lock contention")
+        }
+    }
+
+    fn try_lock(&self) -> bool {
+        let was_locked = self.locked.replace(true);
+        !was_locked
+    }
+
+    unsafe fn unlock(&self) {
+        self.locked.set(false)
     }
 }
