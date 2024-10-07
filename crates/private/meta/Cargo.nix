ef9faf81bcd5c88673267a1394017fc15f0b8a11
Nick Spinale <nick@nickspinale.com>
2024-10-06 23:24:03 -0700

    crates/meta: Add more re-exports
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/private/meta/Cargo.nix
+++ crates/private/meta/Cargo.nix
@@ -20,21 +20,41 @@ mk {
       sel4-async-block-io-fat
       sel4-async-io
       sel4-async-network
+      sel4-async-network-rustls
+      sel4-async-network-rustls-utils
       sel4-async-single-threaded-executor
       sel4-async-time
       sel4-async-unsync
       sel4-atomic-ptr
       sel4-bounce-buffer-allocator
+      sel4-dlmalloc
+      sel4-driver-interfaces
+      sel4-elf-header
       sel4-immediate-sync-once-cell
       sel4-immutable-cell
+      sel4-initialize-tls
       sel4-logging
+      sel4-newlib
       sel4-one-ref-cell
+      sel4-panicking
+      sel4-panicking-env
+      sel4-reset
       sel4-shared-ring-buffer
       sel4-shared-ring-buffer-block-io
       sel4-shared-ring-buffer-block-io-types
       sel4-shared-ring-buffer-bookkeeping
       sel4-shared-ring-buffer-smoltcp
+      sel4-stack
       sel4-sync
+      sel4-sync-trivial
+
+      sel4-bcm2835-aux-uart-driver
+      sel4-pl011-driver
+      sel4-pl031-driver
+      sel4-sp804-driver
+      sel4-virtio-blk
+      sel4-virtio-hal-impl
+      sel4-virtio-net
     ;
 
     sel4-externally-shared = localCrates.sel4-externally-shared // { features = [ "unstable" ]; };
