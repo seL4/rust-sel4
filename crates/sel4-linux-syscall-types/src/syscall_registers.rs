ceb913458b34e900fc16b55c4fbddce64655fc4b
Nick Spinale <nick@nickspinale.com>
2024-10-23 02:04:00 -0700

    crates/sel4-linux-syscall-types: Improve API language
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-linux-syscall-types/src/syscall_registers.rs
+++ crates/sel4-linux-syscall-types/src/syscall_registers.rs
@@ -1,83 +1,89 @@
 use core::ffi::VaList;
 
-pub type SyscallRegisterWord = isize;
+pub type SyscallWordArg = usize;
 
-pub trait SyscallRegisters {
-    fn next_register_word(&mut self) -> Option<SyscallRegisterWord>;
-
-    fn next_register_value<T>(&mut self) -> Option<T>
-    where
-        T: SyscallRegisterValue,
-    {
-        self.next_register_word().map(T::from_word)
-    }
+pub trait SyscallArg {
+    fn from_word(word: SyscallWordArg) -> Self;
 }
 
-pub trait SyscallRegisterValue {
-    fn from_word(word: isize) -> Self;
-}
-
-macro_rules! impl_syscall_register_value {
+macro_rules! impl_syscall_arg {
     ($t:ty) => {
-        impl SyscallRegisterValue for $t {
-            fn from_word(word: isize) -> Self {
+        impl SyscallArg for $t {
+            fn from_word(word: SyscallWordArg) -> Self {
                 word as Self
             }
         }
     };
 }
 
-impl_syscall_register_value!(isize);
-impl_syscall_register_value!(usize);
-impl_syscall_register_value!(i32);
-impl_syscall_register_value!(u32);
-impl_syscall_register_value!(i16);
-impl_syscall_register_value!(u16);
-impl_syscall_register_value!(i8);
-impl_syscall_register_value!(u8);
+impl_syscall_arg!(isize);
+impl_syscall_arg!(usize);
+impl_syscall_arg!(i32);
+impl_syscall_arg!(u32);
+impl_syscall_arg!(i16);
+impl_syscall_arg!(u16);
+impl_syscall_arg!(i8);
+impl_syscall_arg!(u8);
 
 #[cfg(target_pointer_width = "64")]
-impl_syscall_register_value!(i64);
+impl_syscall_arg!(i64);
 
 #[cfg(target_pointer_width = "64")]
-impl_syscall_register_value!(u64);
+impl_syscall_arg!(u64);
 
-impl<T> SyscallRegisterValue for *const T {
-    fn from_word(word: isize) -> Self {
+impl<T> SyscallArg for *const T {
+    fn from_word(word: SyscallWordArg) -> Self {
         word as Self
     }
 }
 
-impl<T> SyscallRegisterValue for *mut T {
-    fn from_word(word: isize) -> Self {
+impl<T> SyscallArg for *mut T {
+    fn from_word(word: SyscallWordArg) -> Self {
         word as Self
     }
 }
 
-pub struct IteratorAsSyscallRegisters<T>(T);
+pub trait SyscallArgs {
+    fn next_word_arg(&mut self) -> Option<SyscallWordArg>;
 
-impl<T> IteratorAsSyscallRegisters<T> {
-    pub unsafe fn new(it: T) -> Self {
+    fn next_arg<T>(&mut self) -> Option<T>
+    where
+        T: SyscallArg,
+    {
+        self.next_word_arg().map(T::from_word)
+    }
+}
+
+impl<T: SyscallArgs + ?Sized> SyscallArgs for &mut T {
+    fn next_word_arg(&mut self) -> Option<SyscallWordArg> {
+        T::next_word_arg(self)
+    }
+}
+
+pub struct IteratorAsSyscallArgs<T>(T);
+
+impl<T> IteratorAsSyscallArgs<T> {
+    pub fn new(it: T) -> Self {
         Self(it)
     }
 }
 
-impl<T: Iterator<Item = SyscallRegisterWord>> SyscallRegisters for IteratorAsSyscallRegisters<T> {
-    fn next_register_word(&mut self) -> Option<SyscallRegisterWord> {
+impl<T: Iterator<Item = SyscallWordArg>> SyscallArgs for IteratorAsSyscallArgs<T> {
+    fn next_word_arg(&mut self) -> Option<SyscallWordArg> {
         self.0.next()
     }
 }
 
-pub struct VaListAsSyscallRegisters<'a, 'f>(VaList<'a, 'f>);
+pub struct VaListAsSyscallArgs<'a, 'f>(VaList<'a, 'f>);
 
-impl<'a, 'f> VaListAsSyscallRegisters<'a, 'f> {
+impl<'a, 'f> VaListAsSyscallArgs<'a, 'f> {
     pub unsafe fn new(va_list: VaList<'a, 'f>) -> Self {
         Self(va_list)
     }
 }
 
-impl<'a, 'f> SyscallRegisters for VaListAsSyscallRegisters<'a, 'f> {
-    fn next_register_word(&mut self) -> Option<SyscallRegisterWord> {
+impl<'a, 'f> SyscallArgs for VaListAsSyscallArgs<'a, 'f> {
+    fn next_word_arg(&mut self) -> Option<SyscallWordArg> {
         Some(unsafe { self.0.arg() })
     }
 }
