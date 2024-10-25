077d49d8dcd50e15d91604de8166568b45377774
Nick Spinale <nick@nickspinale.com>
2024-10-24 17:19:05 -0700

    crates/sel4-backtrace: Improve addr2line context helper
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-backtrace/addr2line-context-helper/src/lib.rs
+++ crates/sel4-backtrace/addr2line-context-helper/src/lib.rs
@@ -8,15 +8,30 @@
 
 extern crate alloc;
 
-use alloc::borrow::Cow;
 use alloc::rc::Rc;
 
 use addr2line::Context as AbstractContext;
 use object::{Object, ObjectSection};
 
-pub use addr2line::gimli::Error;
-
 pub type Context = AbstractContext<gimli::EndianRcSlice<gimli::RunTimeEndian>>;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum Error {
+    ObjectError(object::Error),
+    GimliError(gimli::Error),
+}
+
+impl From<object::Error> for Error {
+    fn from(err: object::Error) -> Self {
+        Self::ObjectError(err)
+    }
+}
+
+impl From<gimli::Error> for Error {
+    fn from(err: gimli::Error) -> Self {
+        Self::GimliError(err)
+    }
+}
 
 pub fn new_context<'data: 'file, 'file, O: Object<'data>>(
     file: &'file O,
@@ -28,31 +43,29 @@ pub fn new_context_with_sup<'data: 'file, 'file, O: Object<'data>>(
     file: &'file O,
     sup_file: Option<&'file O>,
 ) -> Result<Context, Error> {
-    let endian = if file.is_little_endian() {
-        gimli::RunTimeEndian::Little
-    } else {
-        gimli::RunTimeEndian::Big
+    let endian = match file.endianness() {
+        object::Endianness::Little => gimli::RunTimeEndian::Little,
+        object::Endianness::Big => gimli::RunTimeEndian::Big,
     };
-
-    fn load_section<'data: 'file, 'file, O, Endian>(
-        id: gimli::SectionId,
-        file: &'file O,
-        endian: Endian,
-    ) -> Result<gimli::EndianRcSlice<Endian>, Error>
-    where
-        O: Object<'data>,
-        Endian: gimli::Endianity,
-    {
-        let data = file
-            .section_by_name(id.name())
-            .and_then(|section| section.uncompressed_data().ok())
-            .unwrap_or(Cow::Borrowed(&[]));
-        Ok(gimli::EndianRcSlice::new(Rc::from(&*data), endian))
-    }
-
     let mut dwarf = gimli::Dwarf::load(|id| load_section(id, file, endian))?;
     if let Some(sup_file) = sup_file {
         dwarf.load_sup(|id| load_section(id, sup_file, endian))?;
     }
-    Context::from_dwarf(dwarf)
+    Ok(Context::from_dwarf(dwarf)?)
+}
+
+fn load_section<'data: 'file, 'file, O, Endian>(
+    id: gimli::SectionId,
+    file: &'file O,
+    endian: Endian,
+) -> Result<gimli::EndianRcSlice<Endian>, Error>
+where
+    O: Object<'data>,
+    Endian: gimli::Endianity,
+{
+    let data = match file.section_by_name(id.name()) {
+        Some(section) => section.uncompressed_data()?,
+        None => Default::default(),
+    };
+    Ok(gimli::EndianRcSlice::new(Rc::from(data), endian))
 }
