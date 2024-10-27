7374d03db7990d6277c4423e5535701f4f390fee
Nick Spinale <nick@nickspinale.com>
2024-10-27 05:18:45 -0700

    crates/sel4/sys: Make clang invocation more robust
    Map architectures to known-supported targets, rather than expect clang
    to parse our custom target spec names.
    
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4/sys/build/c.rs
+++ crates/sel4/sys/build/c.rs
@@ -37,10 +37,19 @@ pub fn generate_rust(
     libsel4_include_dirs: impl Iterator<Item = impl AsRef<Path>>,
     extra_blocklist: &[String],
 ) -> bindgen::Bindings {
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let clang_target = match target_arch.as_str() {
+        "aarch64" => "aarch64-unknown-none",
+        "arm" => "armv7a-none-eabi",
+        "x86_64" => "x86_64-unknown-none-elf",
+        arch => arch,
+    };
+
     let mut builder = bindgen::Builder::default()
         .header_contents("wrapper.h", HEADER_CONTENTS)
         .detect_include_paths(false)
         .clang_args(libsel4_include_dirs.map(|d| format!("-I{}", d.as_ref().display())))
+        .clang_arg(format!("--target={clang_target}"))
         .ignore_functions();
 
     for item in BLOCKLIST.iter() {
@@ -49,14 +58,6 @@ pub fn generate_rust(
 
     for item in extra_blocklist.iter() {
         builder = builder.blocklist_item(item);
-    }
-
-    {
-        // TODO remove once https://github.com/rust-lang/rust-bindgen/pull/2751 is released
-        let target = env::var("TARGET").unwrap();
-        if let Some(rest) = target.strip_prefix("riscv64imac-") {
-            builder = builder.clang_arg(format!("--target=riscv64-{}", rest));
-        }
     }
 
     builder
