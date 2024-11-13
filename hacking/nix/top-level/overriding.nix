e5754740e2b5aa9a1439e4a89be19652cde6fe7e
Nick Spinale <nick@nickspinale.com>
2024-11-12 22:44:21 -0800

    nix: Divide top-level into multiple files
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

---
+++ hacking/nix/top-level/overriding.nix
@@ -0,0 +1,57 @@
+#
+# Copyright 2024, Colias Group, LLC
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+self: with self;
+
+{
+
+  overrideNixpkgsArgs = f: self.override (superArgs: selfBase:
+    let
+      concreteSuperArgs = superArgs selfBase;
+    in
+      concreteSuperArgs // {
+        nixpkgsArgsFor = crossSystem: f (concreteSuperArgs.nixpkgsArgsFor crossSystem);
+      }
+  );
+
+  withOverlays = overlays: self.overrideNixpkgsArgs (superNixpkgsArgs:
+    superNixpkgsArgs // {
+      overlays = superNixpkgsArgs.overlays ++ overlays;
+    }
+  );
+
+  withConfigOverride = f: self.withOverlays [
+    (self': super': {
+      this = super'.this.overrideScope (self'': super'': {
+        overridableScopeConfig = f super''.overridableScopeConfig;
+      });
+    })
+  ];
+
+  withClippy = self.withConfigOverride (attrs: attrs // {
+    runClippyDefault = true;
+  });
+
+  withFerrocene = self.withConfigOverride (attrs: attrs // {
+    rustEnvironmentSelector = (attrs.rustEnvironmentSelector or {}) // {
+      tracks = "ferrocene";
+      upstream = false;
+    };
+  });
+
+  withVerus = self.withConfigOverride (attrs: attrs // {
+    rustEnvironmentSelector = (attrs.rustEnvironmentSelector or {}) // {
+      tracks = "verus";
+    };
+  });
+
+  withUpstream = self.withConfigOverride (attrs: attrs // {
+    rustEnvironmentSelector = (attrs.rustEnvironmentSelector or {}) // {
+      upstream = true;
+    };
+  });
+
+}
