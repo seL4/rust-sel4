6e8d55efbebdf4bfd98545c767fed5e168bf32bd
Nick Spinale <nick@nickspinale.com>
2025-02-28 10:57:21 -0800

    Bump Verus
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- hacking/nix/scope/verus/default.nix
+++ hacking/nix/scope/verus/default.nix
@@ -27,17 +27,13 @@ let
 
   rustToolchain = assembleRustToolchain {
     inherit channel;
-    sha256 = "sha256-e4mlaJehWBymYxJGgnbuCObVlqMlQSilZ8FljG9zPHY=";
+    sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
   };
 
   rustEnvironment = lib.fix (self: elaborateRustEnvironment (mkDefaultElaborateRustEnvironmentArgs {
     inherit rustToolchain;
   } // {
     inherit channel;
-    backwardsCompatibilityHacks = {
-      outDirInsteadOfArtifactDir = true;
-      noLibraryWorkspace = true;
-    };
     mkCustomTargetPath = mkMkCustomTargetPathForEnvironment {
       rustEnvironment = self;
     };
@@ -45,7 +41,7 @@ let
 
   src = sources.fetchGit {
     url = "https://github.com/coliasgroup/verus.git";
-    rev = "c1d8b986315b1d7fcaa0bf63c2e0497fbebab231"; # branch dev
+    rev = "85249163e18625abaf1e0822c81d0839cc348ac4"; # branch dev
   };
 
   lockfile = vendorLockfile {
