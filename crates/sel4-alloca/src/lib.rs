7c44f061a09d1a9969535dbbb2b243e5093fdbe8
Nick Spinale <nick@nickspinale.com>
2025-03-16 03:43:27 -0700

    crates/sel4-alloca: Fully support returning a value
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-alloca/src/lib.rs
+++ crates/sel4-alloca/src/lib.rs
@@ -14,7 +14,7 @@ use core::slice;
 use cfg_if::cfg_if;
 
 // TODO:
-// - return generic instead of just !
+// - support unwinding
 
 pub fn with_alloca_bytes<R, F: FnOnce(&mut [MaybeUninit<u8>]) -> R>(layout: Layout, f: F) -> R {
     with_alloca_ptr(layout, |p| {
@@ -95,7 +95,7 @@ extern "C" {
     );
 }
 
-macro_rules! common_asm {
+macro_rules! common_asm_prefix {
     () => {
         r#"
             .section .text
@@ -108,65 +108,131 @@ macro_rules! common_asm {
 cfg_if! {
     if #[cfg(target_arch = "aarch64")] {
         global_asm! {
-            common_asm!(),
+            common_asm_prefix!(),
             r#"
+                // preamble
+                stp fp, lr, [sp, #-16]!
+                mov fp, sp
+
                 mov x9, sp
                 sub x9, x9, x0        // x0: reservation_size
                 and x9, x9, x1        // x1: reservation_align_down_mask
                 mov x10, x9           // save reservation_start for later
                 and x9, x9, ~(16 - 1) // align stack
                 mov sp, x9
+
                 mov x0, x10           // pass reservation_start
                 mov x1, x3            // pass cont_arg
-                br x2                 // call cont_fn
+                blr x2                // call cont_fn
+
+                // postamble
+                mov sp, fp
+                ldp fp, lr, [sp], #16
+                ret
             "#
         }
     } else if #[cfg(target_arch = "arm")] {
         global_asm! {
-            common_asm!(),
+            common_asm_prefix!(),
             r#"
+                // preamble
+                push {{r11, lr}}
+                add r11, sp, #0
+
                 mov r4, sp
                 sub r4, r4, r0        // r0: reservation_size
                 and r4, r4, r1        // r1: reservation_align_down_mask
                 mov r6, r4            // save reservation_start for later
                 and r4, r4, ~(4 - 1)  // align stack
                 mov sp, r4
+
                 mov r0, r6            // pass reservation_start
                 mov r1, r3            // pass cont_arg
-                bx r2                 // call cont_fn
+                blx r2                // call cont_fn
+
+                // postamble
+                sub sp, r11, #0
+                pop {{r11, pc}}
             "#
         }
-    } else if #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))] {
+    } else if #[cfg(target_arch = "riscv64")] {
         global_asm! {
-            common_asm!(),
+            common_asm_prefix!(),
             r#"
+                // preamble
+                sd ra, -8(sp)
+                sd s0, -16(sp)
+                mv s0, sp
+                addi sp, sp, -16
+
                 mv t0, sp
                 sub t0, t0, a0        // a0: reservation_size
                 and t0, t0, a1        // a1: reservation_align_down_mask
                 mv t1, t0             // save reservation_start for later
                 and t0, t0, ~(16 - 1) // align stack
                 mv sp, t0
+
                 mv a0, t1             // pass reservation_start
                 mv a1, a3             // pass cont_arg
-                jr a2                 // call cont_fn
+                jalr a2               // call cont_fn
+
+                // postamble
+                mv sp, s0
+                ld ra, -8(sp)
+                ld s0, -16(sp)
+                ret
+            "#
+        }
+    } else if #[cfg(target_arch = "riscv32")] {
+        global_asm! {
+            common_asm_prefix!(),
+            r#"
+                // preamble
+                sw ra, -4(sp)
+                sw s0, -8(sp)
+                mv s0, sp
+                addi sp, sp, -16
+
+                mv t0, sp
+                sub t0, t0, a0        // a0: reservation_size
+                and t0, t0, a1        // a1: reservation_align_down_mask
+                mv t1, t0             // save reservation_start for later
+                and t0, t0, ~(16 - 1) // align stack
+                mv sp, t0
+
+                mv a0, t1             // pass reservation_start
+                mv a1, a3             // pass cont_arg
+                jalr a2               // call cont_fn
+
+                // postamble
+                mv sp, s0
+                lw ra, -4(sp)
+                lw s0, -8(sp)
+                ret
             "#
         }
     } else if #[cfg(target_arch = "x86_64")] {
         global_asm! {
-            common_asm!(),
+            common_asm_prefix!(),
             r#"
+                // preamble
+                push rbp
+                mov rbp, rsp
+
                 mov r10, rsp
                 sub r10, rdi          // rdi: reservation_size
                 and r10, rsi          // rsi: reservation_align_down_mask
                 mov rax, r10          // save reservation_start for later
                 and r10, ~(16 - 1)    // align stack
                 mov rsp, r10
+
                 mov rdi, rax          // pass reservation_start
                 mov rsi, rcx          // pass cont_arg
-                mov rbp, rsp
-                sub rsp, 0x8          // preserve stack alignment
-                push rbp
                 call rdx              // call cont_fn
+
+                // postamble
+                leave
+                ret
             "#
         }
     } else {
