96be56a30237d7754ced094be11d6102ed37f368
Nick Spinale <nick@nickspinale.com>
2025-03-21 17:54:09 -0700

    Introduce sddf-sys crate
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

---
+++ crates/sddf/sys/src/lib.rs
@@ -0,0 +1,10 @@
+//
+// Copyright 2025, Colias Group, LLC
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+#![no_std]
+#![allow(non_camel_case_types)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
