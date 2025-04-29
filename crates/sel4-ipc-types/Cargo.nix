fa32af173f4bed04c230611a554759af3903d472
Nick Spinale <nick@nickspinale.com>
2025-04-28 23:01:54 -0700

    Remove zerocopy and postcard from sel4-ipc-types
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-ipc-types/Cargo.nix
+++ crates/sel4-ipc-types/Cargo.nix
@@ -4,26 +4,13 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-{ mk, versions, localCrates, serdeWith, postcardWith }:
+{ mk, versions, localCrates }:
 
 mk {
   package.name = "sel4-ipc-types";
   dependencies = {
-    zerocopy = {
-      version = versions.zerocopy;
-      optional = true;
-    };
-    serde = serdeWith [] // {
-      optional = true;
-    };
-    postcard = postcardWith [] // {
-      optional = true;
-    };
     sel4-microkit-base = localCrates.sel4-microkit-base // {
       optional = true;
     };
-  };
-  features = {
-    postcard = [ "dep:postcard" "dep:serde" ];
   };
 }
