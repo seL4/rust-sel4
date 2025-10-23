2c8d86e7f279466d33cbc0785cd81d68725f357f
Nick Spinale <nick@nickspinale.com>
2025-10-23 01:18:44 -0700

    Adapt to new atomic intrinsic types
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-shared-memory/build.rs
+++ crates/sel4-shared-memory/build.rs
@@ -5,15 +5,30 @@
 //
 
 fn main() {
-    let old_intrinsics_key = "old_intrinsics";
-    let keys = &[old_intrinsics_key];
+    let before_const_generic_ordering_key = "before_const_generic_ordering";
+
+    // see https://github.com/rust-lang/rust/commit/de1b999ff6c981475e4491ea2fff1851655587e5
+    let before_move_integer_pointer_cast_key = "before_move_integer_pointer_cast";
+
+    let keys = &[
+        before_const_generic_ordering_key,
+        before_move_integer_pointer_cast_key,
+    ];
     for key in keys {
         println!("cargo::rustc-check-cfg=cfg({key})");
     }
+
     if rustversion::cfg!(any(
         all(not(nightly), before(1.89)),
         all(nightly, before(2025 - 06 - 9))
     )) {
-        println!("cargo::rustc-cfg={old_intrinsics_key}");
+        println!("cargo::rustc-cfg={before_const_generic_ordering_key}");
+    }
+
+    if rustversion::cfg!(any(
+        all(not(nightly), before(1.91)),
+        all(nightly, before(2025 - 08 - 9))
+    )) {
+        println!("cargo::rustc-cfg={before_move_integer_pointer_cast_key}");
     }
 }
