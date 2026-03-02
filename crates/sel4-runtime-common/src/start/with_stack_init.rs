10136f23daaf93a78866a905798ebae7254dcdad
Nick Spinale <nick@nickspinale.com>
2026-03-02 03:18:44 -0800

    Use asm_cfg in sel4_runtime_common
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-runtime-common/src/start/with_stack_init.rs
+++ crates/sel4-runtime-common/src/start/with_stack_init.rs
@@ -6,8 +6,6 @@
 
 use core::arch::global_asm;
 
-use cfg_if::cfg_if;
-
 #[macro_export]
 macro_rules! declare_stack {
     ($size:expr) => {
@@ -34,117 +32,74 @@ macro_rules! declare_entrypoint_with_stack_init {
             }
         };
 
-        // TODO make this much prettier once #[feature(asm_cfg)] stabilizes
-        $crate::_private::cfg_if! {
-            if #[cfg(any(target_arch = "aarch64", target_arch = "arm"))] {
-                $crate::_private::global_asm! {
-                    r#"
-                        .extern __sel4_runtime_common__stack_init
+        $crate::_private::global_asm! {
+            r#"
+                .extern __sel4_runtime_common__stack_init
 
-                        .section .text
+                .section .text
 
-                        .global _start
-                        _start:
-                            b __sel4_runtime_common__stack_init
-                    "#
-                }
-            } else if #[cfg(any(target_arch = "riscv64", target_arch = "riscv32"))] {
-                $crate::_private::global_asm! {
-                    r#"
-                        .extern __sel4_runtime_common__stack_init
-
-                        .section .text
-
-                        .global _start
-                        _start:
-                            j __sel4_runtime_common__stack_init
-                    "#
-                }
-            } else if #[cfg(target_arch = "x86_64")] {
-                $crate::_private::global_asm! {
-                    r#"
-                        .extern __sel4_runtime_common__stack_init
-
-                        .section .text
-
-                        .global _start
-                        _start:
-                            jmp __sel4_runtime_common__stack_init
-                    "#
-                }
-            } else {
-                compile_error!("unsupported architecture");
-            }
+                .global _start
+                _start:
+            "#,
+            #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+            r#"
+                    b __sel4_runtime_common__stack_init
+            "#,
+            #[cfg(any(target_arch = "riscv64", target_arch = "riscv32"))]
+            r#"
+                    j __sel4_runtime_common__stack_init
+            "#,
+            #[cfg(target_arch = "x86_64")]
+            r#"
+                    jmp __sel4_runtime_common__stack_init
+            "#,
         }
     };
 }
 
-macro_rules! common_asm_prefix {
-    () => {
-        r#"
-            .extern __sel4_runtime_common__stack_bottom
-            .extern __sel4_runtime_common__call_rust_entrypoint
+global_asm! {
+    r#"
+        .extern __sel4_runtime_common__stack_bottom
+        .extern __sel4_runtime_common__call_rust_entrypoint
 
-            .section .text.__sel4_runtime_common__stack_init, "ax", %progbits
+        .section .text.__sel4_runtime_common__stack_init, "ax", %progbits
 
-            .global __sel4_runtime_common__stack_init
-            __sel4_runtime_common__stack_init:
-        "#
-    };
-}
-
-cfg_if! {
-    if #[cfg(target_arch = "aarch64")] {
-        global_asm! {
-            common_asm_prefix!(),
-            r#"
-                    ldr x9, =__sel4_runtime_common__stack_bottom
-                    ldr x9, [x9]
-                    mov sp, x9
-                    b __sel4_runtime_common__rust_entrypoint
-            "#
-        }
-    } else if #[cfg(target_arch = "arm")] {
-        global_asm! {
-            common_asm_prefix!(),
-            r#"
-                    ldr r8, =__sel4_runtime_common__stack_bottom
-                    ldr r8, [r8]
-                    mov sp, r8
-                    b __sel4_runtime_common__rust_entrypoint
-            "#
-        }
-    } else if #[cfg(target_arch = "riscv64")] {
-        global_asm! {
-            common_asm_prefix!(),
-            r#"
-                    la sp, __sel4_runtime_common__stack_bottom
-                    ld sp, (sp)
-                    j __sel4_runtime_common__rust_entrypoint
-            "#
-        }
-    } else if #[cfg(target_arch = "riscv32")] {
-        global_asm! {
-            common_asm_prefix!(),
-            r#"
-                    la sp, __sel4_runtime_common__stack_bottom
-                    lw sp, (sp)
-                    j __sel4_runtime_common__rust_entrypoint
-            "#
-        }
-    } else if #[cfg(target_arch = "x86_64")] {
-        global_asm! {
-            common_asm_prefix!(),
-            r#"
-                    mov rsp, __sel4_runtime_common__stack_bottom
-                    mov rbp, rsp
-                    sub rsp, 0x8 // Stack must be 16-byte aligned before call
-                    push rbp
-                    call __sel4_runtime_common__rust_entrypoint
-                1:  jmp 1b
-            "#
-        }
-    } else {
-        compile_error!("unsupported architecture");
-    }
+        .global __sel4_runtime_common__stack_init
+        __sel4_runtime_common__stack_init:
+    "#,
+    #[cfg(target_arch = "aarch64")]
+    r#"
+            ldr x9, =__sel4_runtime_common__stack_bottom
+            ldr x9, [x9]
+            mov sp, x9
+            b __sel4_runtime_common__rust_entrypoint
+    "#,
+    #[cfg(target_arch = "arm")]
+    r#"
+            ldr r8, =__sel4_runtime_common__stack_bottom
+            ldr r8, [r8]
+            mov sp, r8
+            b __sel4_runtime_common__rust_entrypoint
+    "#,
+    #[cfg(target_arch = "riscv64")]
+    r#"
+            la sp, __sel4_runtime_common__stack_bottom
+            ld sp, (sp)
+            j __sel4_runtime_common__rust_entrypoint
+    "#,
+    #[cfg(target_arch = "riscv32")]
+    r#"
+            la sp, __sel4_runtime_common__stack_bottom
+            lw sp, (sp)
+            j __sel4_runtime_common__rust_entrypoint
+    "#,
+    #[cfg(target_arch = "x86_64")]
+    r#"
+            mov rsp, __sel4_runtime_common__stack_bottom
+            mov rbp, rsp
+            sub rsp, 0x8 // Stack must be 16-byte aligned before call
+            push rbp
+            call __sel4_runtime_common__rust_entrypoint
+        1:  jmp 1b
+    "#,
 }
