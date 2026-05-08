59c5b0c9a7fee133c36d019613dd40b8c7c36f80
Nick Spinale <nick@nickspinale.com>
2026-05-07 17:42:49 -0700

    Factor out sel4-no-allocator
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-capdl-initializer/src/lib.rs
+++ crates/sel4-capdl-initializer/src/lib.rs
@@ -9,15 +9,15 @@
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
+#[cfg(not(feature = "alloc"))]
+use sel4_no_allocator as _;
+
 mod cslot_allocator;
 mod error;
 mod hold_slots;
 mod initialize;
 mod lib_main;
 mod memory;
-
-#[cfg(not(feature = "alloc"))]
-mod no_allocator;
 
 #[sel4::sel4_cfg(all(ARCH_RISCV64, not(PT_LEVELS = "3")))]
 compile_error!("unsupported configuration");
