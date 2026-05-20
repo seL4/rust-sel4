bc9afd52e426e75fa3a263e62bdf009c3c17af18
Nick Spinale <nick@nickspinale.com>
2026-05-20 15:09:56 -0700

    sel4-kernel-loader: Use cfg_select
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/sel4-kernel-loader/src/arch/mod.rs
+++ crates/sel4-kernel-loader/src/arch/mod.rs
@@ -4,25 +4,18 @@
 // SPDX-License-Identifier: BSD-2-Clause
 //
 
-use sel4_config::{sel4_cfg_if, sel4_cfg_usize};
+use sel4_config::sel4_cfg_usize;
 
-sel4_cfg_if! {
-    if #[sel4_cfg(ARCH_ARM)] {
-        #[path = "arm/mod.rs"]
-        mod imp;
-    } else if #[sel4_cfg(ARCH_RISCV)] {
-        #[path = "riscv/mod.rs"]
-        mod imp;
+cfg_select! {
+    any(target_arch = "arm", target_arch = "aarch64") => {
+        mod arm;
+        pub(crate) use arm::*;
+    }
+    any(target_arch = "riscv32", target_arch = "riscv64") => {
+        mod riscv;
+        pub(crate) use riscv::*;
     }
 }
-
-// HACK for rustfmt
-#[cfg(false)]
-mod arm;
-#[cfg(false)]
-mod riscv;
-
-pub(crate) use imp::*;
 
 pub(crate) trait Arch {
     fn physical_to_logical_core_id(physical_core_id: usize) -> Option<usize> {
